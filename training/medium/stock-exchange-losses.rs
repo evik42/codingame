406cef1daf6b124d636746627f00ee0ba14edc5a
Attila Mravik <attila.mravik@prezi.com>
2018-04-08 22:56:39 +0200

    https://www.codingame.com/training/medium/stock-exchange-losses

---
+++ training/medium/stock-exchange-losses.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let n = parse_input!(input_line, i32);
+    let mut inputs = String::new();
+    io::stdin().read_line(&mut inputs).unwrap();
+    let mut max: i32 = 0;
+    let mut loss: i32 = 0;
+    for i in inputs.split(" ") {
+        let v = parse_input!(i, i32);
+        max = if v > max {v} else {max};
+        loss = if v - max < loss {v - max} else {loss};
+    }
+
+    // Write an action using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("{}", loss);
+}
